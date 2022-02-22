a3aee7e6faa1268cc4c5126f2642cb837eaa2cdf
Albert Salgueda <salguedainsti@gmail.com>
2022-02-22 11:51:01 -0500

    bug fixed

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 #API File
 
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
    return "This is oktopus.io APIs"
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
 
 # https://aws.amazon.com/getting-started/hands-on/serve-a-flask-app/
