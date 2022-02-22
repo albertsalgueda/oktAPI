daa9d9c8cfc07dfde43205fe057d2367a416e190
Albert Salgueda <salguedainsti@gmail.com>
2022-02-22 14:48:47 -0500

    try endpoint sample

--- application.py
+++ application.py
@@ -8,21 +8,22 @@ def say_hello(username = "dear username"):
 header_text = '''
     <html>\n<head> <title>OKTOPUS</title> </head>\n<body>'''
 instructions = '''
-    <p><em>Hint</em>: This is the official oktopus.io API </p>\n'''
+    <p> This is the official oktopus.io API, read the documentation below </p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@application.route("/allocate")
+def sample():
+    campaigns = {
+        1: 0.25, 
+        2: 10.3, 
+        3: 0.25,
+        4: 0.2
+    }
+    return campaigns
 
 # run the app.
 if __name__ == "__main__":
