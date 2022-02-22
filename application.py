c4df25e529a9632424d8a14596f2d6f59ab273b0
Albert Salgueda <salguedainsti@gmail.com>
2022-02-22 14:42:13 -0500

    app changes

--- application.py
+++ application.py
@@ -1,16 +1,14 @@
 from flask import Flask
 
 # print a nice greeting.
-def say_hello(username = "World"):
+def say_hello(username = "dear username"):
     return '<p>Hello %s!</p>\n' % username
 
 # some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+    <html>\n<head> <title>OKTOPUS</title> </head>\n<body>'''
 instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
+    <p><em>Hint</em>: This is the official oktopus.io API </p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
