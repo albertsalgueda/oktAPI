a7c89e00c7aeb92b9163c962070bccc34cad012f
Albert Salgueda <salguedainsti@gmail.com>
2022-02-22 14:25:14 -0500

    fix

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route('/')
-def main():
+def hello():
    return "This is oktopus.io APIs"
 
-if __name__ == '__main__':
-   application.run(debug=True)
+
 # https://aws.amazon.com/getting-started/hands-on/serve-a-flask-app/
