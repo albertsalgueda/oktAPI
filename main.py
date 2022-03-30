8229d93e2604d8497cb9423b383ac6740411f125
Albert Salgueda <salguedainsti@gmail.com>
2022-03-30 10:58:59 -0500

    simplified AI

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ connector = DBConnector()
 class AI(object):
 
   def __init__(self, env, initial_q, initial_visits):
-    print(env)
     self.env = env
     self.initial_q = initial_q
     self.initial_visits = initial_visits
@@ -18,28 +17,17 @@ class AI(object):
     self.q_values = np.ones(self.env.k_arms) * self.initial_q
     self.arm_counts = np.ones(self.env.k_arms) * self.initial_visits
     self.arm_rewards = np.zeros(self.env.k_arms)
-    
-    self.rewards = [0.0]
-    self.cum_rewards = [0.0]
 
   def act(self):
-    count = 0
-    old_estimate = 0.0
+
     arm = np.argmax(self.q_values)
     reward = self.env.take_action(arm,self.q_values)
-    print(f'The rewards at timestamp {self.env.current_time} is {reward}')
     #sum one to the arm that was choosen 
     self.arm_counts[arm] = self.arm_counts[arm] + 1
     #assign rewards for all arms
     for arm in range(self.env.k_arms):
       self.arm_rewards[arm] = self.arm_rewards[arm] + reward[arm]
       self.q_values[arm] = self.q_values[arm] + (1/self.arm_counts[arm]) * (reward[arm] - self.q_values[arm])
-    #print(self.q_values)
-    self.rewards.append(sum(reward))
-    count += 1
-    current_estimate = old_estimate + (1/count)*(sum(reward)-old_estimate)
-    self.cum_rewards.append(current_estimate)
-    old_estimate = current_estimate
 
     return {"arm_counts": self.arm_counts, "rewards": self.rewards, "cum_rewards": self.cum_rewards}
 
